5775c4bf1a4ddada7dea10db533ff7602d858444
Joe 2005 <buckybabaian@gmail.com>
2021-05-31 20:56:37 -0500

    Rewrote sizzle.cmd as a directory specific script.

--- sizzle.cmd
+++ sizzle.cmd
@@ -4,22 +4,21 @@
 ::works more like razzle.cmd than sizzle.bat does.
 ::Expect a lot of parameter changes in this one compared to sizzle.bat
 ::Created 5-27-21
+::Rewritten on 5-31-21
 
 ::TODO: add detection for whether or not we are running within a VS command prompt window. 
+::TODO: add a way for the script to know what has or has not been executed,
+::including asking to redirect build to configure to configure the source tree before executing build
+::TODO: Add a execution date and time logger for all events executed...
 
 ::Variables
-::change these to match your directory or maybe I will just add the code to allow you to change it when executing this
-::too lazy right now
-
-:parameters
-::presetting parameters
-set RoSBEDir=
-set RoSSrcDir=
-::unused parameters for now
-set solutionsDir=
 set solutionsGen=false
 set configure=false
 set build=false
+set srcTree=%cd%
+::set the two variables needed for ReactOS
+set BISON_PKGDATADIR=%srcTree%\tools\RoSBE\share\bison
+set M4=%srcTree%\tools\RoSBE\bin\m4.exe
 
 ::parameters that we can use
 ::help parameters
@@ -28,80 +27,68 @@ if "%1" == "-?" goto Usage
 if "%1" == "/?" goto Usage
 if "%1" == "-help" goto Usage
 if "%1" == "/help" goto Usage
-::directory parameters
-if /I "%1" == "RoSBE_dir" set RosBEDir=%2
-if /I "%3" == "RoSSrc_dir" set RoSSrcDir=%4
-::unused parameters for now
-::do we really need these variables to be set or can we just work with a bunch of goto commands which can execute each other?
-::we need to choose whether to set variables and use if commands or to use variables and goto commands... 
-::if /I "%1" == "solutions_dir" set solutionsDir=%2
-::if /I "%2" == "genSolutions" set solutionsGen=true& set configure=true& set build=false& goto generateSolutions
-::if /I "%3" == "configure" set configure=true& set solutionsGen=false& goto configureSource
-::if /I "%4" == "build" set build=true& set solutionsGen=false& set configure=true& goto buildSource
-
-:printPaths
-echo %RosBEDir%
-echo %RosSrcDir%
-
-:checkDirDefaults
-::if the variables are blank, don't use them and use our defaults, which will be straight
-::from react OS's website
-::this code isn't really needed anymore...
-if "%RosBEDir%" == "" (
-    set RosBEDir=C:\RosBEDir
-    ::tell the idiot he/she didn't set the environments are we are using defaults
-    echo ReactOS build environment not set. Using default directory!
-)
-if "%RosSrcDir%" == "" (
-    set RoSSrcDir=C:\RoSSrcDir
-    ::again, tell them no var was set
-    echo ReactOS source directory not set. Using default directory!
-)
-
-:setVars
-::this code is going to be completely redone to a certain structure, which will be as follows: See notes.
-::make our directory? or add code that can see if it exists already or not
-mkdir %RosSrcDir%\VSSolutions
-::move to our directory
-pushd %RosSrcDir%\VSSolutions
-::set BISON variable
-set BISON_PKGDATADIR=%RosBEDir%\share\bison
-::set M4 variable
-set M4=%RosBEDir%\bin\m4.exe
-
-:solutionChoice
-::our new choice picker whether to execute and generate solution files or tell the user how to
-set /P solutionMakerChoice=Generate Visual Studio solutions now[Y/N][Y]?
-if /I "%solutionMakerChoice%" EQU "Y" goto :generate_solutions
-if /I "%solutionMakerChoice%" EQU "N" goto :exitRunCommand
-if /I "%solutionMakerChoice%" EQU "" goto :generate_solutions
-
-
-::functions are PLACED HERE
+::parameters
+if /I "%1" == "genSolutions" set solutionsGen=true& set configure=false& set build=false& goto :generateSolutions
+if /I "%1" == "configure" set configure=true& set solutionsGen=false& goto :configureSource
+if /I "%1" == "build" set build=true& set solutionsGen=false& set configure=true& goto :buildSource
+if /I "%1" == "state" goto :varStatus
 
 ::help menu
 :Usage
 echo ------------------------------------------------------------------------------------------------------------
 echo Sizzle Help Menu
 echo Valid Parameters:
-echo 1. RoSBE_dir: sets the build environment directory. Defaults to C:\RoSBE, ex: RoSBE_dir=C:\RosBE
-echo 2. RoSSrc_dir: sets the ReactOS source directory. Defaults to C:\RosSrcDir, ex: RoSSrc_dir=C:\reactos-master
-echo 3. Help: displays parameters and info for sizzle.bat
-echo Unused parameters as of now:
-echo 1. configure: Disables generation of Visual Studio solutions and runs configre.cmd to generate ReactOS Build Environment for MSVC
-echo 2. build: Builds all components. See notes.
-echo 3. makecd: generates the ISO images.
+echo 1. Help: displays parameters and info for sizzle.bat
+echo 2. genSolutions: generate solutions for ReactOS and visual studio. 
+echo 2. configure: Disables generation of Visual Studio solutions and runs configre.cmd to generate ReactOS Build Environment for MSVC
+echo 3. build: Builds all components. See notes.
+echo 4. makeImg: Generates disc images. Example: makeImg.cmd
+echo 5. state: displays what variables are on or off, type sizzle_state for more info. 
+::echo 4. makecd: generates the ISO images.
 echo ------------------------------------------------------------------------------------------------------------
 goto :exit
 
-::generate solutions call
-:generate_solutions
-%RosSrcDir%\configure.cmd VSSolution -DENABLE_ROSTESTS=1 -DENABLE_ROSAPPS=1
+:: ---------------- MAIN CODE ------------------------------------------------------------------------------
+::just all of our functions, not much else
+:generateSolutions
+mkdir %srcTree%\VSSolutions
+pushd %srcTree%\VSSolutions
+%srcTree%\configure.cmd VSSolution -DENABLE_ROSTESTS=1 -DENABLE_ROSAPPS=1
+goto :exit
 
-::exit mode
-::Tell a dumb dumb what to do
-:exitRunCommand
-echo run %RosSrcDir%\configure.cmd VSSolution -DENABLE_ROSTESTS=1 -DENABLE_ROSAPPS=1 to generate solutions. 
+:configureSource
+%srcTree%\configure.cmd
+goto :exit
+
+:buildSource
+if %platform% == x64 (
+    cd %srcTree%\output-vs-amd64
+)
+if %platform% == x86 (
+    cd %srcTree%\output-vs-i386
+)
+%srcTree%\tools\RoSBE\bin\ninja.exe all
+goto :makeCdExec
+
+::generates the script to generate the disc images...
+:makeCdExec
+echo @echo off >> makeImg.cmd
+echo ::Script auto generated by sizzle.cmd on completion of the build command. >> makeImg.cmd
+echo ::Generated on %date% at %time% by sizzle.cmd in directory %srcTree%. >> makeImg.cmd
+echo >> makeImg.cmd
+echo ::Generate the disc images. >> makeImg.cmd
+echo ninja hybridcd >> makeImg.cmd
+goto :exit
+::----------------------------------------------------------------------------------------------------------
+
+:varStatus
+echo BISON_PKGDATADIR=%BISON_PKGDATADIR%
+echo M4=%M4%
+echo solutionsGen=%solutionsGen%
+echo configure=%configure%
+echo build=%build%
+echo srcTree=%srcTree%
+echo srcTreeStatic=%srcTreeStatic%, use srcStatic_info for more info about this, or see notes. 
 goto :exit
 
 ::just exits the program, simple
@@ -118,6 +105,8 @@ pause
 ::ISO images, again, might be set into an external script to simplify sizzle and operate similar to the build
 ::parameter, we will see though. 
 
+
+::these notes are useless mostly, but I will just keep them for preservation's sake
 ::variable notes and path structure
 ::our directory for this script will be as follows
 ::
@@ -133,7 +122,6 @@ pause
 ::buildInit.cmd: Would be useful, but variables are in sizzle so.. pointless I guess?
 ::Should we put RoSBE in the root of the source tree, dump its contents into tools or
 ::should we just put the RoSBE directory into the tools directory...
-::
 ::
 ::
 ::
